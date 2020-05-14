be4a0d3225d7f5eb8e1125f1d0093cf4a6d3f51e
Jure Cuhalev <jure@cuhalev.com>
2020-05-14 19:36:53 +0200

    Basic graphic animation

--- components/GraphicOrange.tsx
+++ components/GraphicOrange.tsx
@@ -1,18 +1,70 @@
-import React, { FunctionComponent } from "react";
+/*
+ * Parallax code example from:
+ * https://ironeko.com/posts/parallax-effect-with-react-spring-how-to/
+ */
+import React, {
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 import PieChartColour from "../assets/svg/piechart_colour.svg";
 import BarChartPink from "../assets/svg/barchart_pink.svg";
 
-const GraphicOrange: FunctionComponent = ({}) => (
-  <div className="graphic-orange" aria-hidden={true}>
-    <div className="browser1">
-      <img src="/img/browser.png" alt="" aria-hidden={true} />
-      <PieChartColour />
-    </div>
-    <div className="browser2">
-      <img src="/img/browser.png" alt="" aria-hidden={true} />
-      <BarChartPink />
-    </div>
-  </div>
-);
+import { useSpring, animated as a, interpolate, animated } from "react-spring";
+
+export function debounce(func, wait = 5, immediate = true) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const GraphicOrange: FunctionComponent = ({}) => {
+  const [scrollY, setScrollY] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => setScrollY(window.scrollY);
+    window.addEventListener("scroll", debounce(handleScroll));
+    return () => window.removeEventListener("scroll", debounce(handleScroll));
+  }, [debounce]);
+
+  const [{ springscrollY }, springsetScrollY] = useSpring(() => ({
+    springscrollY: 0,
+  }));
+  const parallaxLevel = 20;
+  springsetScrollY({ springscrollY: scrollY });
+  const interpBrowser1 = springscrollY.interpolate(
+    (o) => `translateY(${o / -30}px)`
+  );
+  const interpBrowser2 = springscrollY.interpolate(
+    (o) => `translateX(${o / -15}px)`
+  );
+
+  return (
+    <animated.div
+      className="graphic-orange"
+      aria-hidden={true}
+      onScroll={(e) => console.log(e)}
+    >
+      <animated.div className="browser1" style={{ transform: interpBrowser1 }}>
+        <img src="/img/browser.png" alt="" aria-hidden={true} />
+        <PieChartColour />
+      </animated.div>
+      <animated.div className="browser2" style={{ transform: interpBrowser2 }}>
+        <img src="/img/browser.png" alt="" aria-hidden={true} />
+        <BarChartPink />
+      </animated.div>
+    </animated.div>
+  );
+};
 
 export default GraphicOrange;
