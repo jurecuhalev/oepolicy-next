b905c7c5d3f5ca82540ace7031e95c0a8f45e7e2
Jure Cuhalev <jure@cuhalev.com>
2020-06-18 10:29:50 +0200

    Remove barchart legend / labels clicking / opens in new window

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -1,13 +1,13 @@
-import Plot from "react-plotly.js"
-import { d3 } from "plotly.js"
-import React, { FunctionComponent, useRef } from "react"
-import { getURL } from "../utils/urls"
+import Plot from "react-plotly.js";
+import { d3 } from "plotly.js";
+import React, { FunctionComponent, useRef } from "react";
+import { getURL } from "../utils/urls";
 
 // https://stackoverflow.com/a/51506718/141200
 const wrap = (s: string, w: number): string =>
-  s.replace(new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, "g"), "$1\n")
+  s.replace(new RegExp(`(?![^\\n]{1,${w}}$)([^\\n]{1,${w}})\\s`, "g"), "$1\n");
 
-const sanitizeLabel = (s: string): string => s.replace(/<[^>]+>/g, " ")
+const sanitizeLabel = (s: string): string => s.replace(/<[^>]+>/g, " ");
 
 const buildFinalUrl = (
   filterName: string,
@@ -20,40 +20,30 @@ const buildFinalUrl = (
       "filter.about.@type": "Policy",
       [filterName]: [urlMapping ? urlMapping[label] : label],
     },
-  })
-}
+  });
+};
 
 const BarChart: FunctionComponent<{
-  x: number[]
-  y: string[]
-  text: string[]
-  fieldName: string
-  title: string
-  urlMapping?: { name: string; id: string }
+  x: number[];
+  y: string[];
+  text: string[];
+  fieldName: string;
+  title: string;
+  urlMapping?: { name: string; id: string };
 }> = ({ x, y, text, fieldName, title, urlMapping }) => {
-  const plotRef = useRef(null)
+  const plotRef = useRef(null);
 
   // Plotly doesn't support word wrap on legend items, so we manually add <br /> line breaks
   // (that we of have to later strip with `sanitizeLabel`
-  y = y.map((item) => wrap(item, 20).replace(/(\r\n|\n|\r)/gm, "<br />"))
+  y = y.map((item) => wrap(item, 20).replace(/(\r\n|\n|\r)/gm, "<br />"));
 
   function handleClick(e) {
     if (e.points) {
-      const point = e.points[0]
-      const label = sanitizeLabel(point.label)
-      window.open(buildFinalUrl(fieldName, label, urlMapping), "_blank")
-      return false
+      const point = e.points[0];
+      const label = sanitizeLabel(point.label);
+      window.open(buildFinalUrl(fieldName, label, urlMapping), "_blank");
+      return false;
     }
-  }
-
-  const addExtraClickEvents = () => {
-    d3.selectAll(".yaxislayer-above")
-      .selectAll("text")
-      .on("click", function (d) {
-        const label = sanitizeLabel(d.text)
-        window.open(buildFinalUrl(fieldName, label, urlMapping), "_blank")
-        return false
-      })
   }
 
   return (
@@ -121,10 +111,9 @@ const BarChart: FunctionComponent<{
           displayModeBar: false,
         }}
         onClick={handleClick}
-        onAfterPlot={addExtraClickEvents}
       />
     </div>
-  )
-}
+  );
+};
 
-export default BarChart
+export default BarChart;
