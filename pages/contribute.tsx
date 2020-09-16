eb462c90b1087344a606868b7f7eb94265959929
Jure Cuhalev <jure@cuhalev.com>
2020-09-16 12:05:33 +0200

    Contribute page

---
+++ pages/contribute.tsx
@@ -0,0 +1,41 @@
+import React, { FunctionComponent } from "react";
+import Layout from "../components/Layout";
+import ContentBlock from "../components/ContentBlock";
+
+import ContributePageMd, {
+  frontMatter as contributePage,
+} from "../docs/601-contribute.mdx";
+import PolicyPageMd, {
+  frontMatter as policyPage,
+} from "../docs/602-policy.mdx";
+import CaseStudyPageMd, {
+  frontMatter as caseStudyPage,
+} from "../docs/603-casestudy.mdx";
+
+import Hero from "../components/Hero";
+
+const ImprintPage: FunctionComponent = () => (
+  <Layout
+    title="Who we are & What we do"
+    hero={
+      <Hero background="orange">
+        <h1 className="text-5xl font-sans">
+          Help us <b>grow the collective knowledge</b> on how to implement Open
+          Education!
+        </h1>
+      </Hero>
+    }
+  >
+    <ContentBlock {...contributePage}>
+      <ContributePageMd />
+    </ContentBlock>
+    <ContentBlock {...policyPage}>
+      <PolicyPageMd />
+    </ContentBlock>
+    <ContentBlock {...caseStudyPage}>
+      <CaseStudyPageMd />
+    </ContentBlock>
+  </Layout>
+);
+
+export default ImprintPage;
