1e1d2947f5094a7e6ebe6a6ba6f805e108cea375
Jure Cuhalev <jure@cuhalev.com>
2020-12-02 17:00:17 +0100

    Refactor country stats to work on netlify

--- pages/explore/country.tsx
+++ pages/explore/country.tsx
@@ -81,13 +81,12 @@ const BarChartPoliciesByScope = dynamic(
 
 const ExplorePage: FunctionComponent = () => {
   const router = useRouter();
-  const countryCode = router.query["country"]?.toString()?.toUpperCase();
+  const countryCode = router.query["code"]?.toString()?.toUpperCase();
   const countryName = countries[countryCode];
 
   const url = `https://oerworldmap.org/resource.json?q=about.@type:Policy&sort=dateCreated:DESC&size=500&filter.feature.properties.location.address.addressCountry=%5B"${countryCode}"%5D`;
   const servicesUrl = `https://oerworldmap.org/resource.json?field=feature.properties.location.address.addressRegion&filter.about.@type=%22Service%22&filter.about.additionalType.@id=%5B%22https%3A%2F%2Foerworldmap.org%2Fassets%2Fjson%2Fservices.json%23referatory%22%2C%22https%3A%2F%2Foerworldmap.org%2Fassets%2Fjson%2Fservices.json%23repository%22%5D&sort=dateCreated:DESC&size=500&filter.feature.properties.location.address.addressCountry=%5B"${countryCode}"%5D`;
 
-  console.log(url);
   const { data } = useSWR(url, fetcher, {
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
